ab3bf02fea18ba6aeed68afe4c7d551896333bf0
Maxim Tsaregradtsev <3m3rcy3@gmail.com>
2024-07-25 18:19:33 +0300

    fix imports, update README.md

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,3 +1,3 @@
-from .charity_project import router as charity_project
-from .donation import router as donation
-from .user import router as user
+from .charity_project import router as charity_project # noqa
+from .donation import router as donation # noqa
+from .user import router as user # noqa
